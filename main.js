0408ca9cf740734c0ef8730b7bf940a5dc50339e
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-26 06:54:24 +0300

    Задание 1: Базовые операции с текстом

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'use strict';
+const text = document.getElementById('text-element');
+const originalText = document.getElementById('text-element').textContent;
+function changeText(){
+    text.textContent =  'Текст был изменен!';
+}
+
+function addText(){
+    text.textContent += '(дополнено)';
+}
+
+function resetText(){
+    text.textContent = originalText;
+}
