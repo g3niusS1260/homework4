1a24b51b727176be5c09f21b7da8b38f9ec60df0
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-27 14:17:36 +0300

    Код к задаче №2

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function safeOutput(){
+    let safeOutp = document.getElementById('safe-output');
+    console.log(safeOutp.textContent);
+}
+
+function unsafeOutput(){
+    let unsfOutp = document.getElementById('unsafe-output');
+    console.log(unsfOutp.innerHTML);
+}
