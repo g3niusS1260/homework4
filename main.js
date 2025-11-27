069802e66e1bca26215dd59a465c37f1c0d05de2
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-27 15:19:45 +0300

    Код к задаче №4

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict';
+const styleBlock = document.getElementById('style-demo');
+const originalStyles = styleBlock.style.cssText;
+
+function changeColor(){
+
+    styleBlock.style.color = 'lightblue';
+}
+
+function changeSize(){
+
+    styleBlock.style.width = '300px';
+    styleBlock.style.height = '100px';
+}
+
+function addBorder(){
+
+    styleBlock.style.cssText += `
+    border-radius: 20px;
+    box-shadow: 3px 3px 10px black;
+    `;
+}
+
+function resetStyles(){
+    styleBlock.style.cssText = originalStyles;
+}
