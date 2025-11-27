a823d1aa4dab2effa6fc5dd00f86add4ca420d18
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-27 14:29:33 +0300

    Код к задаче №3

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function addSimpleContent(){
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = 'Новый текстовый блок';
+
+    document.body.append(newParagraph);
+}
+
+function addHTMLContent(){
+    const newBlock = `
+    <div class="newBlock">
+        <h2>Динамически добавленный заголовок</h2>
+    </div>`;
+
+    document.body.innerHTML += newBlock;
+}
+
+function clearContent(){
+    const container = document.getElementById('dynamic-content');
+
+    container.innerHTML = null;
+}
