2cf9642bd4575fad67f7756b8a00bcc74959e9c5
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-11-28 06:46:03 +0300

    feat: функции updateTitle(), updateBio(), updateName()

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function updateName(){
+    const inputName = document.getElementById('name-input');
+
+    const nameAtTheMoment = document.getElementById('user-name');
+
+    nameAtTheMoment.innerText = inputName.value;
+}
+
+function updateTitle(){
+    const inputTitle = document.getElementById('title-input');
+
+    const titleAtTheMoment = document.getElementById('user-title');
+
+    titleAtTheMoment.innerText = inputTitle.value;
+}
+
+function updateBio(){
+    const inputBio = document.getElementById('bio-input');
+
+    const bioAtTheMoment = document.getElementById('user-bio');
+
+    bioAtTheMoment.innerText = inputBio.value;
+}
